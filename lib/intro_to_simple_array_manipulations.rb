7346e53e93961ad5c316de2637e5db4d305b2f59
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-08 18:18:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,3 +33,10 @@ def shift_with_args(array)
   p ice_cream_brands
   p ice_cream
 end
+
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  all_my favs = my_favorite_things.concat(more_favs)
+  p all_my_favs
+end
