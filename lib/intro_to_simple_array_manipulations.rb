c66f313f37bdb41572c847133a2b78c9ae367217
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-08 18:31:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -37,6 +37,6 @@ end
 def using_concat(my_favorite_things, more_favs)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
-  my_favorite_thigns.concat(more_favs)
+  my_favorite_things.concat(more_favs)
   p my_favorite_things
 end
